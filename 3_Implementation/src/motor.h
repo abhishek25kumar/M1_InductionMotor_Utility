daa5a866918e62afd37956931045de768d613aa0
abhishek25kumar <98945487+abhishek25kumar@users.noreply.github.com>
2022-03-03 23:41:54 +0530

    Add files via upload

---
+++ 3_Implementation/src/motor.h
@@ -0,0 +1,20 @@
+/**
+ * @file motor.h
+ * @author Abhishek Kumar (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+#ifndef MOTOR_H_
+#define MOTOR_H_
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#define pi 3.1415
+#endif
+
